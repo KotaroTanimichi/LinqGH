fd9a02f7e2b0413e2301975afad20c2c0149ab29
Kotaro Tanimichi <notKoutaro@gmail.com>
2022-06-22 03:25:57 +0900

    Fixed file name.

---
+++ Linquornis/LinqComponents/Select.cs
@@ -0,0 +1,34 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Special;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper;
+
+namespace Linquornis.LinqComponents
+{
+    public class Select : LinqComponentBase
+    {
+
+        public Select(IGH_InstanceDescription tag) : base(tag)
+        {
+        }
+
+        public Select(IGH_InstanceDescription tag, GH_ParamAccess access) : base(tag, access)
+        {
+        }
+
+        public Select() : base("Select", "Select", "")
+        {
+        }
+
+        public override Guid ComponentGuid => new Guid("63f614bf-e09f-41a9-8418-430463dc0936");
+
+        protected override IEnumerable<object> Evaluate(IEnumerable<object> values, string lambdaExpression)
+        {
+            return values.SelectDynamic(LinqExpression).ToArray();
+        }
+    }
+}
--- Linquornis/LinqComponents/Where.cs
+++ Linquornis/LinqComponents/Where.cs
@@ -1,11 +1,7 @@
 ﻿using Grasshopper.Kernel;
-using Grasshopper.Kernel.Data;
-using Grasshopper.Kernel.Types;
-using Grasshopper.Kernel.Special;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Grasshopper;
 
 namespace Linquornis.LinqComponents
 {
@@ -29,28 +25,6 @@ namespace Linquornis.LinqComponents
         protected override IEnumerable<object> Evaluate(IEnumerable<object> values, string lambdaExpression)
         {
             return values.WhereDynamic(LinqExpression).ToArray();
-        }
-    }
-    public class Select : LinqComponentBase
-    {
-
-        public Select(IGH_InstanceDescription tag) : base(tag)
-        {
-        }
-
-        public Select(IGH_InstanceDescription tag, GH_ParamAccess access) : base(tag, access)
-        {
-        }
-
-        public Select() : base("Select", "Select", "")
-        {
-        }
-
-        public override Guid ComponentGuid => new Guid("63f614bf-e09f-41a9-8418-430463dc0936");
-
-        protected override IEnumerable<object> Evaluate(IEnumerable<object> values, string lambdaExpression)
-        {
-            return values.SelectDynamic(LinqExpression).ToArray();
         }
     }
 }
